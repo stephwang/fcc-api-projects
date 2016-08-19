8586cbaef0dbe7370e1aa8c4a95725cc70b3bdae
Steph Wang <sw.steph@gmail.com>
2016-08-19 06:56:46 +0000

    working timestamp

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var moment = require('moment');
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:time', function(req, res){
+    var time = req.params.time;
+    var result = {
+      unix: null
+      , natural: null
+    }
+    
+    // convert to date object
+    var date = new Date(time);
+    
+    // try timestamp in milliseconds if the above doesn't work
+    if (isNaN(date)){
+      date = new Date(time * 1000);
+    }
+    
+    if(moment(date).isValid()){
+      result.unix = moment(date).unix();
+      result.natural = moment(date).format('MMMM D, YYYY');
+    }
+    
+    res.send(result);
 });
+
+app.listen(8080);
